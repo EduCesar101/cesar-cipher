a2240b1954635521ec2c5c8f8f7a79d7f7e7e2be
Eduardo Souza <educesarrs@hotmail.com>
2025-04-15 22:57:04 -0300

    Finished!

--- src/App.tsx
+++ src/App.tsx
@@ -27,47 +27,49 @@ function App() {
 
   return (
     <>
-      <div className="main">
-        
-      <h1>Cifra de César</h1>
-        <form onSubmit={handleSubmit}>
+    <main>
       <div className="card">
-        <label htmlFor="">Texto Normal</label>
-        <br />  
-        <textarea name="nText" id="message" value={message} onChange={(e) => setMessage(e.target.value)}
-         placeholder='Digite o texto...' rows={4} cols={35}></textarea>
-        <br />
-        <br />
+        <h1>Cifra de César</h1>
+        <form onSubmit={handleSubmit}>
+        <div className="container">
+          <label htmlFor="">Texto Normal</label>
+          <br />  
+          <textarea name="nText" id="message" value={message} onChange={(e) => setMessage(e.target.value)}
+          placeholder='Digite o texto...' rows={4} cols={35}></textarea>
+          <br />
+          <br />
 
-        <div>
-        <label htmlFor="">Deslocamento</label> <br />
-        <button onClick={() => {
-          if(deslocamento > 0){
-            setDeslocamento(deslocamento - 1)
-          }
-        }}>-</button>
-        <input type="number" value={deslocamento} onChange={(e) => setDeslocamento(e.target.valueAsNumber)} min={0} max={25} maxLength={2} readOnly/>
-        <button onClick={() => {
-          if(deslocamento < 25){
-            setDeslocamento(deslocamento + 1)
-          }
-        }}>+</button>
-        </div>
-      </div>
-      <div className='buttons'>
-        <button type='submit' onClick={() => setInputValue('C')}>Criptografar</button>
-        <button type='submit' onClick={() => setInputValue('D')}>Descriptografar</button>
-      </div>
-      <div className='card'>  
-      <label htmlFor="">Texto Final</label>
-        <br />  
-        <textarea name="cryptText" id="cText" placeholder='Texto Final' value={cText} readOnly rows={4} cols={35}></textarea>
-        <br />
-        <button onClick={() => copyToClipboard()}>Copiar</button>
-      </div>
+            <label htmlFor="">Deslocamento</label>
+          <div className='buttons'>
+            <button onClick={() => {
+              if(deslocamento > 0){
+                setDeslocamento(deslocamento - 1)
+              }
+            }}>-</button>
+            <input type="number" value={deslocamento} onChange={(e) => setDeslocamento(e.target.valueAsNumber)} min={0} max={25} maxLength={2} readOnly/>
+            <button onClick={() => {
+              if(deslocamento < 25){
+                setDeslocamento(deslocamento + 1)
+              }
+            }}>+</button>
+          </div>
+          </div>
+          <div className='buttons'>
+            <button type='submit' onClick={() => setInputValue('C')}>Criptografar</button>
+            <button type='submit' onClick={() => setInputValue('D')}>Descriptografar</button>
+          </div>
+          <div className='container'>  
+            <label htmlFor="">Texto Final</label>
+            <br />  
+            <textarea name="cryptText" id="cText" placeholder='Texto Final' value={cText} readOnly rows={4} cols={35}></textarea>
+            <br />
+            <button onClick={() => copyToClipboard()}>Copiar</button>
+          </div>
 
-      </form>
-    </div>
+        </form>
+
+      </div>
+      </main>
     </>
   )
 }
